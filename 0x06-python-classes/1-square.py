8ee951a784cb298bc0d75d0987f26ecc474e994c
Joe-Chege <njorogejosephchege@gmail.com>
2023-05-31 02:14:50 -0700

    correction and sorting pycode

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 """Define a class Square."""
-
-
 class Square:
     """Represent a square."""
 
@@ -12,4 +10,3 @@ class Square:
             size (int): The size of the new square.
         """
         self.__size = size
-        
