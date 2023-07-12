8a860cef20ecc35acbcf1328df091a3bd6aa98e3
joe-chege <njorogejosephchege@gmail.com>
2023-07-12 02:13:11 -0700

    advanced tasks

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Defines a text file insertion function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts text after each line containing a given string in a file.
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
+        file.truncate()
