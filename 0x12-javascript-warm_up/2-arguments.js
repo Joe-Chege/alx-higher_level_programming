b3ef9ffeaa65792986330402776fc027183ce7e4
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-11 15:24:48 +0300

    Value for argument

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numberOfArguments = process.argv.length - 2;
+if (numberOfArguments === 0) {
+  console.log('No argument');
+} else if (numberOfArguments === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
