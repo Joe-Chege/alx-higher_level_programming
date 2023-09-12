2c3492b4f84c025226eb1f52fce34cacc6ac1d85
Joe-chege <njorogejosephchege@gmail.com>
2023-09-12 04:49:36 -0700

    task one

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,15 +2,9 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
-    }
-    
-    // Initialize the instance attributes width and height
     this.width = w;
     this.height = h;
   }
 }
 
-module.exports = Rectangle; // Export the class for testing purposes
+module.exports = Rectangle;
