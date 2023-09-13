54c7e6b2cdc682b2e6b9b3acd89f832d5d9f2f95
Joe-chege <njorogejosephchege@gmail.com>
2023-09-13 04:58:54 -0700

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-function factorial (n) {
-    if (n < 0) {
-      return (-1);
-    }
-    if (n === 0 || isNaN(n)) {
-      return (1);
-    }
-    return (n * factorial(n - 1));
+
+function computeFactorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
   }
-  
+
+  return n * computeFactorial(n - 1);
+}
+
+const input = Number(process.argv[2]);
+console.log(`Factorial of ${input} is: ${computeFactorial(input)}`);
