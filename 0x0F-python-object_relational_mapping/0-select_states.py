c2d704407cd8703223422fae19411d0701b1d5b9
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-19 13:12:33 +0300

    task one

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""
-Script to list states from a MySQL database.
-"""
+'''
+lists all states from the database hbtn_0e_0_usa
+'''
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        host='localhost')
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        # Execute the SQL query to retrieve states sorted by id
-        cursor.execute("SELECT * FROM states ORDER BY id")
-
-        # Fetch all the results
-        results = cursor.fetchall()
-
-        for state in states:
-            print(state)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-    finally:
-        cursor.close()
-        db.close()
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
