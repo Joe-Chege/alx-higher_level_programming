c9e444b923192bc2b08b1b13f5f080949be50717
Joe-Chege <njorogejosephchege@gmail.com>
2023-09-29 16:30:19 +0300

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes a URL, sends a request, and displays the body of the
+response. It handles urllib.error.HTTPError exceptions and prints the HTTP
+status code in case of an error.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
